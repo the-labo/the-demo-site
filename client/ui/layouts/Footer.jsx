86b6af0186c08deaded433551cd0a872f59927a8
okunishinishi <okunishinishi@gmail.com>
2018-01-22 00:44:07 +0900

    fix error handler

--- client/ui/layouts/Footer.jsx
+++ client/ui/layouts/Footer.jsx
@@ -11,6 +11,7 @@ import { LocaleForm } from '../bounds'
 import { Urls } from '@self/conf'
 import { withLoc } from 'the-loc'
 import { asPure, asBound, compose, } from 'the-hoc'
+import styles from './Footer.pcss'
 
 const Footer = compose(
   asPure,
@@ -19,32 +20,30 @@ const Footer = compose(
                          l
                        }) {
     return (
-      <div>
-        <TheFooter>
-          <div style={{width: '100%'}}>
-            <TheFooter.Row>
-              <div>
-              </div>
-              <div>
-                <LocaleForm/>
-              </div>
-            </TheFooter.Row>
-            <TheFooter.Row>
-              <TheFooter.CopyRight year={2018}
-                                   holder={l('org.ORG_NAME')}
-              />
-              <TheFooter.Links>
-                <TheFooter.Link to={Urls.ABOUT_PRIVACY_POLICY_URL}>
-                  {l('buttons.SHOW_PRIVACY_POLICY')}
-                </TheFooter.Link>
-                <TheFooter.Link to={Urls.ABOUT_TERMS_OF_USE_URL}>
-                  {l('buttons.SHOW_TERMS_OF_USE')}
-                </TheFooter.Link>
-              </TheFooter.Links>
-            </TheFooter.Row>
-          </div>
-        </TheFooter>
-      </div>
+      <TheFooter className={styles.self}>
+        <div className={styles.inner}>
+          <TheFooter.Row>
+            <div>
+            </div>
+            <div>
+              <LocaleForm/>
+            </div>
+          </TheFooter.Row>
+          <TheFooter.Row>
+            <TheFooter.CopyRight year={2018}
+                                 holder={l('org.ORG_NAME')}
+            />
+            <TheFooter.Links>
+              <TheFooter.Link to={Urls.ABOUT_PRIVACY_POLICY_URL}>
+                {l('buttons.SHOW_PRIVACY_POLICY')}
+              </TheFooter.Link>
+              <TheFooter.Link to={Urls.ABOUT_TERMS_OF_USE_URL}>
+                {l('buttons.SHOW_TERMS_OF_USE')}
+              </TheFooter.Link>
+            </TheFooter.Links>
+          </TheFooter.Row>
+        </div>
+      </TheFooter>
     )
   }
 )
