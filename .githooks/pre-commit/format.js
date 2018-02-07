e7bef908006903b2f478043bcac038bccc277cf1
okunishinishi <okunishinishi@gmail.com>
2018-02-07 15:53:44 +0900

    fix fomatter message

--- .githooks/pre-commit/format.js
+++ .githooks/pre-commit/format.js
@@ -13,7 +13,9 @@ void async function () {
   const results = await pon.run('format', {disableLogging: true})
   const filenames = Object.values(results).reduce(flatten).reduce((r, v) => [].concat(r, v))
   if (filenames.length > 0) {
-    console.error(`[COMMIT_REJECTED] Files just formatted. Add the changes and try again (${JSON.stringify(filenames)})`)
+    console.error(
+      `[COMMIT_REJECTED] Some files are just formatted. Add the changes to git and try again ( ${filenames.join(',')} )`
+    )
     process.exit(1)
   }
 
