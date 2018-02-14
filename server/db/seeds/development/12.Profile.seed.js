cfa2ede3be07049777042cd1c82ccf5ae60b9c5a
okunishinishi <okunishinishi@gmail.com>
2018-02-14 19:56:07 +0900

    better test data

--- server/db/seeds/development/12.Profile.seed.js
+++ server/db/seeds/development/12.Profile.seed.js
@@ -2,11 +2,13 @@
 
 const {pascalcase} = require('stringcase')
 const Users = require('./10.User.seed')
+const seed = require('the-seed')('en')
 
 module.exports = [
-  ...Users.map(({id, name}) => ({
+  ...Users.map(({id, name}) => seed.apply({
     email: `${name}@example.com`,
     id,
+    image: '#{image.avatar}',
     name: `[Test] ${pascalcase(name)}`,
     user: {$$as: 'User', $$entity: true, id},
   }))
