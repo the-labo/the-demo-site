4ce693fc229f30a5dbd688b414548643b785c545
okunishinishi <okunishinishi@gmail.com>
2018-02-19 18:48:41 +0900

    refactoring

--- client/ui/wrappers/ofUser.jsx
+++ client/ui/wrappers/ofUser.jsx
@@ -18,18 +18,14 @@ function ofUser (Component) {
   OfUser.wrapStack = wrapStack(OfUser, Component)
 
   return stateful(
-    (state, ownProps) => {
-      return {
-        user: ownProps.user || state['account.entity'],
-      }
-    },
-    (handle, propsProxy) => {
-      return {
-        onReceive ({user}) {
-          user && propsProxy.onUser?.(user)
-        },
-      }
-    }
+    (state, ownProps) => ({
+      user: ownProps.user || state['account.entity'],
+    }),
+    (handle, propsProxy) => ({
+      onReceive ({user}) {
+        user && propsProxy.onUser?.(user)
+      },
+    })
   )(OfUser)
 }
 
